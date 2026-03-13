d5b665a1043a1a62b2415cc570d1b13b1b4fe4ef
Akshansh kaushal <akshanshkaushal5960@gmail.com>
2026-03-13 23:08:51 +0530

    Registration + handling

--- backend/app.js
+++ backend/app.js
@@ -1,31 +1,41 @@
 import express from 'express'
+import cors from "cors";
 
 const app=express();
 
+// apply middleware
+app.use(express.json())
+app.use(cors)
+
+let users = [];
+// home route---
 app.get('/',(req,res)=>{
     res.send('home page successfully login')
 })
+// registering api--
+app.post("/api/register",(req,res)=>{
+    const{username,Password}=req.body;
+
+    // validating the userid or password
+
+    if(!username||!Password){
+        return res.json({msg:"fill it for creation"});
+    }
+    // checking if user already exist
+    const userExists=user.find(user=>user.username===username);
+    if(userExists){
+        return res.json({msg:"already exist"});
+    }
+    
+    // storing user;
+    users.push({username,Password});
+    res.json({msg:"registration successfull"});
+});
 
 
 
-app.get('/api/registrationPage',(req,res)=>{
-    res.send(`
-      <h1>registration Page</h1>
-      <form action="/submit-details" method="POST">
-        <input type="text" name="name" placeholder="UserName"><br><br>
-        <input type="text" name="name" placeholder="Password"><br><br>
-        <button type="submit">Submit</button>
-      </form>
-    `);
-    return res.end(); 
-})
 
-if (req.url === '/submit-details' && req.method === 'POST') {
 
-    const Registration =[
-    {username:[],Password:[]}
-]
-}
 
 
 const PORT=3001;
